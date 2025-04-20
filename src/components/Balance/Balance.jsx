617e66041c88358eafb8e3f69904ac767bec5e5b
Aaditya Sharma <114431922+aadityasharmax@users.noreply.github.com>
2025-04-20 06:06:15 +0000

    ..

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -5,7 +5,7 @@ const Balance = ({balance}) => {
     <div className='flex justify-center mt-10'>
       <div className='bg-[#1e293b] h-[120px] w-[250px] rounded-xl '>
         <h2 className='text-[#cbd5e1] text-center mt-5 text-2xl'>Your Balance :</h2>
-        <h1 className='text-[#ef4444] text-3xl font-bold text-center mt-3'>{`₹${balance}`}</h1>
+        <h1 className='text-green-500 text-3xl font-bold text-center mt-3'>{`₹${balance}`}</h1>
       </div>
     </div>
     
